7cf80f9342d3eb18104605ce94feac5def435cf1
Miles Mezaki <mmezaki@olin.edu>
2022-08-01 16:52:17 -0400

    Make UI Fixes to realtime map. Fix broken links on base html page. Correct API endpoint and add information to calls. Allow for retrieval of east boston sensors.

--- src/access_quantaq.py
+++ src/access_quantaq.py
@@ -4,7 +4,7 @@ from . import quantaq_key
 import os.path
 from datetime import date
 QUANTAQ_APIKEY = quantaq_key.QUANTAQ_APIKEY
-client = quantaq.QuantAQAPIClient(api_key=QUANTAQ_APIKEY)
+client = quantaq.ProductionAPIClient(api_key=QUANTAQ_APIKEY) # this line may need to be changed to a different client depending on quantaq
 
 def list_sensors():
     devices_raw = client.devices.list(filter="city,like,%_oxbury%")
@@ -37,19 +37,20 @@ def update_sensor_list(filename="sensor_list.json", devices_raw=list_sensors()):
     document_device_features(devices_raw, full_path)
     return read_document(full_path)
 
-def get_sensor_ids():
-    sns = {}
-    devices_raw = client.devices.list(filter="city,like,Roxbury")
+def get_sensor_ids(filter, sns={}):
+    devices_raw = client.devices.list(filter=filter)
     for device in devices_raw:
         id = device["id"]
         sn = device["sn"]
-        sns[id] = sn
+        desc = device['description']
+        sns[id] = [sn, desc]
     return sns
 
 def append_sensor_data(data={}):
-    sns = get_sensor_ids()
+    sns = get_sensor_ids("city,like,Roxbury", {})
+    sns = get_sensor_ids("city,like,East Boston", sns)
     for id in sns:
-        data_raw = client.data.list(sn=sns[id], start=str(date.today()), sort="timestamp,asc", limit=1)
+        data_raw = client.data.list(sn=sns[id][0], start=str(date.today()), sort="timestamp,asc", limit=1)
         
         # data_raw = client.data.bydate(sn=sns[id], date=str(date.today()), sort="timestamp,asc", limit=1)
         # data_raw = client.data.get(id=id, sn=sns[id])
@@ -57,9 +58,10 @@ def append_sensor_data(data={}):
             d = data_raw[0]
             data[id] = {'lat': d['geo']['lat'] if d['geo']['lat'] is not None else -1,
                         'lon': d['geo']['lon'] if d['geo']['lon'] is not None else -1,
-                        'pm1': "Good" if d['pm1'] < 2 else "Okay" if d["pm1"] >= 2 and d['pm1'] < 5 else "Bad" if d['pm1'] >= 5 else "None",
-                        'pm25': "Good" if d['pm25'] < 5 else "Okay" if d["pm25"] >= 5 and d['pm25'] < 12.5 else "Bad" if d['pm25'] >= 12.5 else "None",
-                        'pm10': "Good" if d['pm1'] < 20 else "Okay" if d["pm1"] >= 20 and d['pm1'] < 35 else "Bad" if d['pm1'] >= 35 else "None"}
+                        'pm1': [d['pm1'], "Good", "green"] if d['pm1'] < 2 else [d['pm1'], "Okay","yellow"] if d["pm1"] >= 2 and d['pm1'] < 5 else [d['pm1'], "Bad",'red'] if d['pm1'] >= 5 else "None",
+                        'pm25': [d['pm25'], "Good"] if d['pm25'] < 5 else [d['pm25'], "Okay"] if d["pm25"] >= 5 and d['pm25'] < 12.5 else [d['pm25'], "Bad"] if d['pm25'] >= 12.5 else "None",
+                        'pm10': [d['pm10'], "Good"] if d['pm10'] < 20 else [d['pm10'], "Okay"] if d["pm10"] >= 20 and d['pm10'] < 35 else [d['pm10'], "Bad"] if d['pm10'] >= 35 else "None",
+                        'desc': sns[id][1]}
 
             if 'met' in d: # For compatibility with older sensors
                 if d['met'] is not None:
@@ -76,3 +78,4 @@ def append_sensor_data(data={}):
 #     # for i in range(len(teams)):
 #     #     print(teams[i]['id'])
 #     append_sensor_data()
+    
